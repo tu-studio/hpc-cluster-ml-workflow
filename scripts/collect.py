74ea5d71b5f984a7ab7bf946ac06090aa5a45544
Michael Witte <michael-witte@hotmail.de>
2024-06-21 12:41:26 +0200

    Prepare to integrate dvc Keeps old pipeline in collect.py for data collecting tasks Cleans the repository to use dvc pipelines Tests basic dvc commands

--- scripts/collect.py
+++ scripts/collect.py
@@ -1,10 +1,25 @@
+# This script is used to collect files from the internet and save them to the data/raw directory.
+# It reads the configuration from collect_config.yaml file and executes the modules defined in the configuration.
+# It is handled as a pipeline, where each module is executed in sequence.
+# It could be extended to support more modules for data collection tasks.
+
 import os
-# import requests 
-# import subprocess
-# import platform
 import wget
 import zipfile
+import yaml
 
+class ConfigManager:
+    def __init__(self, config_path):
+        with open(config_path, 'r') as file:
+            self.config = yaml.safe_load(file)
+    
+    def get(self, key):
+        return self.config.get(key, None)
+
+class ModuleFactory:
+    def create_module(self, module_type, config):
+        if module_type == "download":
+            return DownloadModule(config)
 
 class DownloadModule:
     def __init__(self, config):
@@ -46,3 +61,22 @@ class DownloadModule:
                 print(f"Error downloading {url}: {e}")
 
         
+def main():
+    config_manager = ConfigManager("collect_config.yaml")
+    factory = ModuleFactory()
+    
+    modules = config_manager.get("collect")
+    print(f"Modules: {modules}")
+    if modules is not None:
+        for module_config in modules:
+            if module_config.get('enabled', True):  # Check if the module is enabled
+                module = factory.create_module(module_config['module'], module_config)
+                if module is not None:
+                    module.execute()
+                else:
+                    print(f"Module {module_config['module']} configuration not found, skipping...")
+    else:
+        print("No modules found in the configuration.")
+
+if __name__ == "__main__":
+    main()
--- scripts/module_factory.py
+++
@@ -1,6 +0,0 @@
-from download import DownloadModule
-
-class ModuleFactory:
-    def create_module(self, module_type, config):
-        if module_type == "download":
-            return DownloadModule(config)
