240207a83327a41b8807d00c4dca8020b8140923
Michael Witte <michael-witte@hotmail.de>
2024-07-08 21:16:48 +0200

    Update batchjob.sh

--- batchjob.sh
+++ batchjob.sh
@@ -22,9 +22,9 @@ module load nvidia/cuda/12.2
 
 module load singularity/4.0.2
 
-singularity pull docker://michaelwitte/ml-training-pipeline:latest
+singularity pull docker://michaelwitte/pipeline-image:latest
 
-STORAGE_DEFAULT_DIRECTORY="$PWD" singularity exec --bind $(pwd):/usr/src/app --bind $HOME/.ssh:/root/.ssh ml-training-pipeline.sif sh -c "ssh-keyscan github.com >> /root/.ssh/known_hosts && git config --global user.name 'michaelwitte' && git config --global user.email 'michael-witte@hotmail.de' && dvc exp run"
+STORAGE_DEFAULT_DIRECTORY="$PWD" singularity exec --bind $(pwd):/usr/src/app --bind $HOME/.ssh:/root/.ssh pipeline-image_latest.sif sh -c "ssh-keyscan github.com >> /root/.ssh/known_hosts && git config --global user.name 'michaelwitte' && git config --global user.email 'michael-witte@hotmail.de' && dvc exp run"
 
 ## if [ $? -ne 0 ]; then
 ##    echo "Error occurred during DVC experiment run; exiting."
