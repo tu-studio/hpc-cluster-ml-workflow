5131181fe47dd2e2412f2be8496806f654ebaf27
Michael Witte <michael-witte@hotmail.de>
2024-07-11 14:37:53 +0200

    Automate experiment-branch push to git- and dvc-remote from cluster

--- batchjob.sh
+++ batchjob.sh
@@ -1,32 +1,36 @@
 #!/bin/bash
 
-#SBATCH -J exp_job      
-
-#SBATCH --ntasks=1                    
-#SBATCH --nodes=1                     
-#SBATCH --ntasks-per-core=1            
-#SBATCH --cpus-per-task=1            
-#SBATCH --gres=gpu:tesla:1            
-
-#SBATCH --mem=100GB                  
-#SBATCH --time=100:00:00            
-#SBATCH --partition=gpu               
-
+#SBATCH -J exp_job
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-core=1
+#SBATCH --cpus-per-task=1
+#SBATCH --gres=gpu:tesla:1
+#SBATCH --mem=100GB
+#SBATCH --time=100:00:00
+#SBATCH --partition=gpu
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user={michael.gh.witte@campus.tu-berlin.de}
 
 ulimit -u 512
 
-## Load cuda module 
+## Load necessary modules
+module load singularity/4.0.2 
+module load nvidia/cuda/12.2 
 
-module load singularity/4.0.2
+## Pull the latest container image
+singularity pull docker://michaelwitte/ml-pipeline-image:latest &&
 
-module load nvidia/cuda/12.2
-
-singularity pull docker://michaelwitte/ml-pipeline-image:latest
-
-STORAGE_DEFAULT_DIRECTORY="$PWD" singularity exec --nv --bind $(pwd):/usr/src/app --bind $HOME/.ssh:/root/.ssh ml-pipeline-image_latest.sif sh -c "ssh-keyscan github.com >> /root/.ssh/known_hosts && git config --global user.name 'michaelwitte' && git config --global user.email 'michael-witte@hotmail.de' && . /usr/src/cntnrvenv/bin/activate && dvc exp run"
-
-
-
-
+## Execute inside the singularity container
+STORAGE_DEFAULT_DIRECTORY="$PWD" singularity exec --nv --bind $(pwd):/usr/src/app --bind $HOME/.ssh:/root/.ssh ml-pipeline-image_latest.sif sh -c "
+  ssh-keyscan github.com >> /root/.ssh/known_hosts &&  # Add GitHub to known hosts to avoid user interaction
+  git config --global user.name 'michaelwitte' &&     # Set global Git config for user name
+  git config --global user.email 'michael-witte@hotmail.de' &&  # Set global Git config for email
+  . /usr/src/cntnrvenv/bin/activate &&                # Activate the environment
+  git pull &&
+  dvc pull data/raw &&
+  dvc exp run &&                                      # Run DVC experiment
+  EXP_ID=\$(dvc exp show --no-pager | grep '* Experiment' | awk '{print \$3}') &&  # Get the experiment ID
+  dvc exp branch \${EXP_ID} exp_\${EXP_ID} &&         # Use DVC to create and checkout a branch for the experiment
+  git push --set-upstream origin exp_\${EXP_ID}       # Push the new branch to remote
+"
