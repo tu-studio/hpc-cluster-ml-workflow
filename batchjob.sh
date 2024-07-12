14a4100515af7ca3336755c31c8ee026dfbc46cd
michaelwitte <michael-witte@hotmail.de>
2024-07-12 18:44:34 +0200

    Automatic branching works Workaround for inaccessible  environment variable: Use slurm.out logs to retrieve current experiment name

--- batchjob.sh
+++ batchjob.sh
@@ -7,34 +7,35 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --gres=gpu:tesla:1
 #SBATCH --mem=100GB
-#SBATCH --time=100:00:00
+#SBATCH --time=1:00:00 
 #SBATCH --partition=gpu
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user={michael.gh.witte@campus.tu-berlin.de}
 
 ulimit -u 512
 
-## Load necessary modules
+# Load necessary modules
 module load singularity/4.0.2 
 module load nvidia/cuda/12.2 
 
-## Pull the latest container image
+# Check and pull the latest container image
+if [ -f ml-pipeline-image_latest.sif ]; then
+    rm ml-pipeline-image_latest.sif
+fi
 singularity pull docker://michaelwitte/ml-pipeline-image:latest 
 
-## Execute inside the singularity container
-STORAGE_DEFAULT_DIRECTORY="$PWD" singularity exec --nv --bind $(pwd):/usr/src/app --bind $HOME/.ssh:/root/.ssh ml-pipeline-image_latest.sif sh -c "
-  ssh-keyscan github.com >> /root/.ssh/known_hosts &&  # Add GitHub to known hosts to avoid user interaction
-  git config --global user.name 'michaelwitte' &&     # Set global Git config for user name
-  git config --global user.email 'michael-witte@hotmail.de' &&  # Set global Git config for email
-  . /usr/src/cntnrvenv/bin/activate &&                # Activate the environment
-  git pull origin main &&
+echo "Starting singularity execution..."
+
+STORAGE_DEFAULT_DIRECTORY="$PWD" singularity exec --nv --bind $(pwd):/usr/src/app --bind $HOME/.ssh:/root/.ssh ml-pipeline-image_latest.sif bash -c '
+  ssh-keyscan github.com >> /root/.ssh/known_hosts &&
+  git config --global user.name "michaelwitte" &&     
+  git config --global user.email "michael-witte@hotmail.de" &&  
+  source /usr/src/cntnrvenv/bin/activate &&                
   dvc pull data/raw &&
-  dvc exp run &&                                      # Run DVC experiment
-  experiment_name=$(tail -f slurm-${SLURM_JOB_ID}.out | grep -oP "Ran experiment\(s\): \K[\w\-]+") &&
-  echo "The current experiment is: $experiment_name" &&
-  dvc exp branch $experiment_name exp_${experiment_name} &&                    # Create a new branch for the experiment
-  git checkout exp_${experiment_name} &&                      # Checkout the new branch
-  dvc checkout &&                                    # Checkout the experiment
-  git push --set-upstream origin exp_${experiment_name}  &&      # Push the new branch to remote
-  dvc push &&
-"
+  dvc exp run &&
+  sleep 5 &&
+  experiment_name=$(grep -oP "Ran experiment\(s\): \K[\w\-]+" slurm-$SLURM_JOB_ID.out) &&
+  dvc exp branch $experiment_name "exp_$experiment_name" &&
+  git checkout "exp_$experiment_name" --force &&
+  dvc checkout &&
+  git push --set-upstream origin "exp_$experiment_name"  &&      # Push the new branch to remote
+  dvc push
+  '
