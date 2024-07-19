a95616e4366808c07e0afd4e1b1361f8f9f93c36
michaelwitte <michael-witte@hotmail.de>
2024-07-19 17:11:28 +0200

    Merging detached head

--- source/utils/save_logs.py
+++ source/utils/save_logs.py
@@ -2,22 +2,24 @@ import os
 import shutil
 
 current_slurm_job_id = os.environ.get('SLURM_JOB_ID')
-if slurm_job_id is None:
+if current_slurm_job_id is None:
     raise EnvironmentError("The environment variable 'SLURM_JOB_ID' is not set.")
 tustu_logs_path = os.environ.get('TUSTU_LOGS_PATH')
 if tustu_logs_path is None: 
     raise EnvironmentError("The environment variable 'TUSTU_LOGS_PATH' is not set.")
 
-slurm_path = os.pathjoin(tustu_logs_path, 'slurm')
+slurm_path = os.path.join(tustu_logs_path, 'slurm')
 exp_slurm_path = 'exp-logs/slurm'
 
 if not os.path.exists(exp_slurm_path):
     os.makedirs(exp_slurm_path)
 
 for f in os.listdir(slurm_path):
-    slurm_id = f.split('-')[1]
-    if slurm_id == current_slurm_job_id:
-        shutil.copy(f, exp_slurm_path)
+    if f.startswith('slurm'):
+        print(f"Checking logfile: {f}")
+        slurm_id = f.split('-')[1].split('.')[0]
+        if slurm_id == current_slurm_job_id:
+            shutil.copy(os.path.join(slurm_path, f), exp_slurm_path)
 
 
     
