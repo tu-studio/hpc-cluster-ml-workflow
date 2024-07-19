7a78a2138724b91727f9a62849957cd42b3b570f
Fares Schulz <schulzfares@gmail.com>
2024-07-19 16:42:08 +0200

    Minor changes

--- source/utils/helpers.py
+++ source/utils/helpers.py
@@ -1,33 +1,26 @@
 import os
 import shutil
 
-def 
+def copy_tensorboard_log(tensorboard_path, destination_path, hostname, timestamp) -> None:
 
-if not os.path.exists('tensorboard-final/'):
-    os.makedirs('tensorboard-final/')
+    if not os.path.exists(destination_path):
+        os.makedirs(destination_path)
 
-
-# Check if there are log files for the current hostname and append them to a list
-log_files = []
-for f in os.listdir(tensorboard_path):
-    parts = f.split('.')
-    # Erzeugt eine temporäre Liste ohne die ersten 3 Elemente und die letzten 2 Elemente
-    # Annahme: Die ersten 3 Teile sind nicht Teil des Hostnamens und die letzten 2 Teile sind die ID und die Erweiterung
-    temp_parts = parts[3:-2]
-    # Wieder zusammensetzen und prüfen, ob der Hostname enthalten ist
-    temp_hostname = '.'.join(temp_parts)
-    if hostname in temp_hostname:
-        log_files.append(f)
-# If there are log files for the current hostname, copy the log file with the closest timestamp to the current time
-if len(log_files) > 0:
-    # Find the log file with the closest timestamp to the current time
-    closest_file = min(log_files, key=lambda x: abs(int(x.split('.')[3]) - int(time_now)))
-    shutil.copy(os.path.join(tensorboard_path, closest_file), 'tensorboard-final/')
-else:
-    print("No log files found for the current hostname.")
-
-print("Done!")
-
-# Save the model
-torch.save(model.state_dict(), "models/checkpoints/" + name + ".pth")
-print("Saved PyTorch Model State to model.pth")
+    # Check if there are log files for the current hostname and append them to a list
+    log_files = []
+    for f in os.listdir(tensorboard_path):
+        parts = f.split('.')
+        # Erzeugt eine temporäre Liste ohne die ersten 3 Elemente und die letzten 2 Elemente
+        # Annahme: Die ersten 3 Teile sind nicht Teil des Hostnamens und die letzten 2 Teile sind die ID und die Erweiterung
+        temp_parts = parts[3:-2]
+        # Wieder zusammensetzen und prüfen, ob der Hostname enthalten ist
+        temp_hostname = '.'.join(temp_parts)
+        if hostname in temp_hostname:
+            log_files.append(f)
+    # If there are log files for the current hostname, copy the log file with the closest timestamp to the current time
+    if len(log_files) > 0:
+        # Find the log file with the closest timestamp to the current time
+        closest_file = min(log_files, key=lambda x: abs(int(x.split('.')[3]) - int(timestamp)))
+        shutil.copy(os.path.join(tensorboard_path, closest_file), destination_path)
+    else:
+        print("No log files found for the current hostname.")
