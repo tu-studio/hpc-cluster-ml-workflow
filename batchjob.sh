cb23019eca000b9e74063c844c52b05d2259a686
michaelwitte <michael-witte@hotmail.de>
2024-07-23 03:08:24 +0200

    Test pipeline

--- batchjob.sh
+++ batchjob.sh
@@ -21,7 +21,7 @@ export $(grep -v '^#' global.env | xargs)
 if [ -f $TUSTU_PROJECT_NAME-image_latest.sif ]; then
   rm $TUSTU_PROJECT_NAME-image_latest.sif
 fi
-Pull the latest docker image from Docker Hub and convert it to a singularity image. Using cached singularity image if nothing changed
+# Pull the latest docker image from Docker Hub and convert it to a singularity image. Using cached singularity image if nothing changed
 singularity pull docker://$TUSTU_DOCKERHUB_USERNAME/$TUSTU_PROJECT_NAME-image:latest 
 
 echo "Starting singularity execution..."
@@ -29,21 +29,21 @@ echo "Starting singularity execution..."
 # Run the singularity container, bind the current directory to the container's working directory, bind ssh key for git
 DEFAULT_DIR="$PWD" singularity exec --nv ml-pipeline-image_latest.sif bash -c '    
   echo "Checking directory existence..."
-  if [ ! -d "../$TUSTU_TEMP_PATH" ]; then
-    mkdir -p "../$TUSTU_TEMP_PATH"
-    echo "The directory ../$TUSTU_TEMP_PATH has been created."
+  if [ ! -d "$TUSTU_TEMP_PATH" ]; then
+    mkdir -p "$TUSTU_TEMP_PATH"
+    echo "The directory $TUSTU_TEMP_PATH has been created."
   else
-    echo "The directory ../$TUSTU_TEMP_PATH already exists."
+    echo "The directory $TUSTU_TEMP_PATH already exists."
   fi
 
-  mkdir "../$TUSTU_TEMP_PATH/$INDEX"
+  mkdir "$TUSTU_TEMP_PATH/$INDEX"
   echo "Copying files..."
   {
     git ls-files;
     echo ".dvc/config.local";
-  } | rsync -av --files-from=- ./ "../$TUSTU_TEMP_PATH/$INDEX"
+  } | rsync -av --files-from=- ./ "$TUSTU_TEMP_PATH/$INDEX"
 
-  cd ../$TUSTU_TEMP_PATH/$INDEX
+  cd $TUSTU_TEMP_PATH/$INDEX
 
   echo "Setting DVC cache directory..."
   dvc cache dir $DEFAULT_DIR/.dvc/cache
