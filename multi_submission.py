ff20ea78093783d7539e7a4e37d3544a9c905264
michaelwitte <michael-witte@hotmail.de>
2024-08-13 12:42:28 +0000

    dvc: commit experiment e2f791c7717c4a80e09f8b41c75f11da47974dcfe89b604ff026a1d6814e9b2c

--- multi_submission.py
+++ multi_submission.py
@@ -3,12 +3,12 @@ import subprocess
 import os 
 
 # Submit experiment for hyperparameter combination
-def submit_batch_job(test_split, batch_size):
+def submit_batch_job(test_split, batch_size, learning_rate):
     # Set dynamic parameters for the batch job as environment variables
     # But dont forget to add the os.environ to the new environment variables otherwise the PATH is not found
     env = {
         **os.environ,
-        "EXP_PARAMS": f"-S preprocess.test_split={test_split} -S train.batch_size={batch_size}"
+        "EXP_PARAMS": f"-S preprocess.test_split={test_split} -S train.batch_size={batch_size} -S train.learning_rate={learning_rate}"
     }
     # Run sbatch command with the environment variables as bash! subprocess! command (otherwise module not found)
     subprocess.run(['/usr/bin/bash', '-c', 'sbatch slurm_job.sh'], env=env)
@@ -16,6 +16,7 @@ def submit_batch_job(test_split, batch_size):
 if __name__ == "__main__":
     test_split_list = [0.2, 0.3]
     batch_size_list = [2048, 4096]
+    learning_rate_list = [0.01, 0.1]
     # Iterate over a cartesian product parameter grid of the test_split and batch_size lists
-    for test_split, batch_size in itertools.product(test_split_list, batch_size_list):
-        submit_batch_job(test_split,batch_size)
+    for test_split, batch_size, learning_rate in itertools.product(test_split_list, batch_size_list, learning_rate_list):
+        submit_batch_job(test_split,batch_size,learning_rate)
