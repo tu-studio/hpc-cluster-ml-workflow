26442086d7102c50b279c16a9487b190086b572d
Michael Witte <michael-witte@hotmail.de>
2024-08-19 19:41:06 +0200

    Adapt model.py import to package approach

--- source/train.py
+++ source/train.py
@@ -2,7 +2,7 @@ import torch
 import torchinfo
 from source.utils import logs, config
 from pathlib import Path
-from model import NeuralNetwork
+from source.model import NeuralNetwork
 
 def train_epoch(dataloader, model, loss_fn, optimizer, device, writer, epoch):
     size = len(dataloader.dataset)
