9a15f6d7be6580cf30af8dbbb339fee3f5a1711f
Fares Schulz <schulzfares@gmail.com>
2024-12-22 13:22:34 +0100

    slurm_job script now has options to force rebuild and use of .sif format for containers

--- multi_submission.py
+++ multi_submission.py
@@ -1,13 +1,16 @@
+#!./venv/bin/python
+
 # Copyright 2024 tu-studio
 # This file is licensed under the Apache License, Version 2.0.
 # See the LICENSE file in the root of this project for details.
 
 import itertools
 import subprocess
-import os 
+import os
+import sys
 
 # Submit experiment for hyperparameter combination
-def submit_batch_job(test_split, batch_size):
+def submit_batch_job(arguments, test_split, batch_size):
     # Set dynamic parameters for the batch job as environment variables
     # But dont forget to add the os.environ to the new environment variables otherwise the PATH is not found
     env = {
@@ -15,11 +18,14 @@ def submit_batch_job(test_split, batch_size):
         "EXP_PARAMS": f"-S preprocess.test_split={test_split} -S train.batch_size={batch_size}"
     }
     # Run sbatch command with the environment variables as bash! subprocess! command (otherwise module not found)
-    subprocess.run(['/usr/bin/bash', '-c', 'sbatch slurm_job.sh'], env=env)
+    subprocess.run(['/usr/bin/bash', '-c', f'sbatch slurm_job.sh {" ".join(arguments)}'], env=env)
 
 if __name__ == "__main__":
+
+    arguments = sys.argv[1:]
+
     test_split_list = [0.2, 0.3]
     batch_size_list = [2048, 4096]
     # Iterate over a cartesian product parameter grid of the test_split and batch_size lists
     for test_split, batch_size in itertools.product(test_split_list, batch_size_list):
-        submit_batch_job(test_split,batch_size)
+        submit_batch_job(arguments, test_split, batch_size)
--- slurm_job.sh
+++ slurm_job.sh
@@ -15,6 +15,56 @@
 #SBATCH --partition=gpu
 #SBATCH --output=./logs/slurm/slurm-%j.out
 
+# Default variable values
+rebuild_container=false
+sif_container=false
+
+# Function to display script usage
+usage() {
+  echo "Usage: $0 [OPTIONS]"
+  echo "Options:"
+  echo " -h, --help                Display this help message"
+  echo " -b, --rebuild-container   Force the rebuild of the singularity container (default: false)"
+  echo " -s, --sif-container       Build the singularity container as SIF (Singularity Image Format) file (default: false)"
+}
+
+# Function to handle options and arguments
+handle_options() {
+  while [ $# -gt 0 ]; do
+    case $1 in
+      -h | --help)
+        usage
+        exit 0
+        ;;
+      -b | --rebuild-container)
+        rebuild_container=true
+        ;;
+      -s | --sif-container)
+        sif_container=true
+        ;;
+      *)
+        echo "Invalid option: $1" >&2
+        usage
+        exit 1
+        ;;
+    esac
+    shift
+  done
+}
+
+# Main script execution
+handle_options "$@"
+
+# Perform the desired actions based on the provided flags and arguments
+if [ "$rebuild_container" = true ]; then
+  echo "Forcing the rebuild of the singularity container..."
+fi
+
+if [ "$sif_container" = true ]; then
+  echo "Singularity container format set to SIF (Singularity Image Format) file..."
+  echo "When executed, the container will be converted to a temporary sandboxed image. This may take a while..."
+fi
+
 # Load necessary modules
 module load singularity/4.0.2
 
@@ -26,14 +76,28 @@ set +o allexport
 # Define DEFAULT_DIR in the host environment
 export DEFAULT_DIR="$PWD"
 
-# Remove the previous singularity image if it exists
-if [ -f $TUSTU_PROJECT_NAME-image_latest.sif ]; then
-  rm $TUSTU_PROJECT_NAME-image_latest.sif
+if [ "$sif_container" = true ]; then
+  container_extension=".sif"
+  container_build_flags=""
+else
+  container_extension="/"
+  container_build_flags="--sandbox"
 fi
-# Pull the latest docker image from Docker Hub and convert it to a singularity image. Using cached singularity image if nothing changed
-singularity pull docker://$TUSTU_DOCKERHUB_USERNAME/$TUSTU_PROJECT_NAME-image:latest
 
-echo "Starting singularity execution..."
+# Remove existing container if --rebuild-container flag is set
+if { [ -d $TUSTU_PROJECT_NAME-image_latest$container_extension ] || [ -f $TUSTU_PROJECT_NAME-image_latest$container_extension ]; } && [ "$rebuild_container" = true ]; then
+  echo "Removing the existing container as --rebuild-container flag is set..."
+  rm -rf $TUSTU_PROJECT_NAME-image_latest$container_extension
+fi
+
+# Build the singularity container from the docker image if it does not exist
+if ! { [ -d $TUSTU_PROJECT_NAME-image_latest$container_extension ] || [ -f $TUSTU_PROJECT_NAME-image_latest$container_extension ]; } ; then
+  echo "Building the singularity container from docker image..."
+  # Pull the latest docker image from Docker Hub and convert it to a singularity image. This will automatically take the a cached image if it exists.
+  singularity build $container_build_flags $TUSTU_PROJECT_NAME-image_latest$container_extension docker://$TUSTU_DOCKERHUB_USERNAME/$TUSTU_PROJECT_NAME-image:latest
+fi
+
+echo "Starting execution from singularity container..."
 
 # Run the singularity container
-singularity exec --nv --bind $DEFAULT_DIR $TUSTU_PROJECT_NAME-image_latest.sif ./exp_workflow.sh
+singularity exec --nv --bind $DEFAULT_DIR $TUSTU_PROJECT_NAME-image_latest$container_extension ./exp_workflow.sh
